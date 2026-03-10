433f7eb383519be938f9dbad3797e3d4df642a05
ganbo <923099645@qq.com>
2026-03-10 20:51:07 +0800

    变量定义

--- go_study/1.变量定义.go
+++ go_study/1.变量定义.go
@@ -1,22 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"goCode/go_study/version"
+)
 
-var age int = 100
+var age int
 
-const address string = "北京"
+var (
+	age1 int = 18
+	age2 int = 19
+)
+
+const qName string = "测试常量定义，定义的时候就要赋值，赋值了就不能改了"
 
 func main() {
 
-	//声明变量
+	// 先定义再赋值
 	var name string
-
-	//再复制
-	name = "hello world"
-
+	name = "张三"
 	fmt.Println(name)
 
-	fmt.Println(age)
+	// 声明变量后就赋值
+	var userName string = "枫枫"
+	fmt.Println(userName)
 
-	fmt.Println(address)
+	var name1, name2, name3 string = "张三", "李四", "王五"
+	fmt.Println(name1, name2, name3)
+
+	a1, a2 := 100, 200
+	fmt.Println(a1, a2)
+
+	fmt.Println(age2)
+	fmt.Println(qName)
+
+	// 输出引用文件的常量
+	fmt.Println(version.Versin1)
 }
---
+++ go_study/version/version.go
@@ -0,0 +1,4 @@
+package version
+
+const Versin1 = "1.11.22"
+const version2 = "2.22.11"
