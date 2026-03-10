66d8d88b8236a6af1b49d58a0247b2b701b9aecd
ganbo <923099645@qq.com>
2026-03-10 23:22:48 +0800

    i++ 和 i--必须独立使用，不能在计算后一步赋值或者比较大小

--- go_study/main.go
+++ go_study/main.go
@@ -1,15 +1,39 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	//var a int = -10
 	//var b int = -3
 	//fmt.Println(a % b)
 
-	for i := 5; i < 20; i++ {
-		fmt.Println(i)
-	}
+	//for i := 5; i < 20; i++ {
+	//	fmt.Println(i)
+	//}
+
+	var days int = 97
+	var week int = days / 7
+	var day int = 97 % 7
+	fmt.Println(week, day)
+
+	var huashi = 134.2
+	var sheshi = 5.0 / 9 * (huashi - 100)
+	fmt.Println(sheshi)
+
+	fmt.Printf("%v", sheshi)
+	fmt.Println("%.2f", sheshi)
+
+	fmt.Println("--------------关系运算符-------------------")
+
+	var n1 int = 9
+	var n2 int = 10
+	fmt.Println(n1 == n2)
+	fmt.Println(n1 != n2)
+	fmt.Println(n1 > n2)
+	fmt.Println(n1 < n2)
+	fmt.Println(n1 >= n2)
+	fmt.Println(n1 <= n2)
+	flag := n1 > n2
+	fmt.Println("flag====>", flag)
+	fmt.Println("--------------逻辑运算符-------------------")
 }
