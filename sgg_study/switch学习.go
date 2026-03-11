47bc4beeabf8bedf0cd013152bbada31287697bb
ganbo <923099645@qq.com>
2026-03-11 20:38:54 +0800

    switch

---
+++ sgg_study/switch学习.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//m1()
+
+	m2()
+}
+
+func m1() {
+
+	var a int = 5566
+
+	switch a {
+	case 1, 2, 10:
+		fmt.Println("a is 1 or 2 or 10")
+	case 55:
+		fmt.Println("a is 55")
+	default:
+		fmt.Println("default================")
+
+	}
+}
+
+func m2() {
+
+	fmt.Println("请输入一个字符，a,b,c,d,e,f,g")
+	var a byte
+	fmt.Scanf("%c", &a)
+	switch a {
+	case 'a', 'b', 'c', 'd', 'e', 'f', 'g':
+		fmt.Println("输入的字符是：", a)
+	default:
+		fmt.Println("输入的字符不是：a,b,c,d,e,f,g")
+	}
+
+}
