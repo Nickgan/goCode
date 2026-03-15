45d86d3d29ab2a3faa71e01bc33fb2aa4f0b2713
ganbo <923099645@qq.com>
2026-03-15 23:27:27 +0800

    输出

--- go_study/1.变量定义.go
+++ go_study/1.变量定义.go
@@ -1,8 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"goCode/go_study/version"
+)
+
+var (
+	name = "全局变量name 张三"
+	age  = 18
+)
 
 func main() {
 
 	fmt.Println("hello world")
+
+	// 1.先定义，在复制
+	var name string
+	name = "张三"
+	fmt.Println("name==========>", name)
+
+	// 2.声明同时赋值，这里可以省略数据类型
+	var age int = 18
+	fmt.Println("age==========>", age)
+
+	// 3.声明并赋值，又叫段声明
+	address := "北京"
+	fmt.Println("address==========>", address)
+
+	fmt.Println("name==========>", name)
+
+	const 常量 = "常量,要用const定义，定义了就不能改了"
+	fmt.Println("name3==========>", 常量)
+
+	fmt.Println("=========================")
+
+	fmt.Printf("引用外面的变量: %v", version.Version2)
 }
---
+++ go_study/version/Version.go
@@ -0,0 +1,4 @@
+package version
+
+const version1 = "首字母小写，私有"
+const Version2 = "首字母大写，共有"
