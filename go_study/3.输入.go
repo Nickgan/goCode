e5923ef7dcdf1b1c3d9e87b12e9332f0c4b262fb
ganbo <923099645@qq.com>
2026-03-15 23:37:07 +0800

    输入

--- go_study/3.输入.go
+++ go_study/3.输入.go
@@ -6,6 +6,9 @@ func main() {
 	var name string
 
 	fmt.Print("请输入你的名字：")
-	fmt.Scan(&name)
+	n, error := fmt.Scan(&name)
 	fmt.Println("你的名字是：", name)
+
+	fmt.Println("n:", n)
+	fmt.Println("error:", error)
 }
