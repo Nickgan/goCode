46ca1dd91a999d281489d397b6c3e4ea6d01f4a3
ganbo <ganbo@qq.com>
2026-03-16 15:32:24 +0800

    切片

---
+++ go_study/5.数组.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var arr [5]string = [5]string{"a", "b", "c", "d", "e"}
+	fmt.Println(arr)
+	fmt.Println(len(arr))
+	fmt.Println(arr[0])
+
+	for i := range arr {
+		fmt.Println(arr[i])
+	}
+
+	fmt.Println("最后一个元素===>", arr[len(arr)-1])
+
+}
---
+++ go_study/6.切片(list).go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var list []int
+
+	list = append(list, 1)
+	list = append(list, 2)
+
+	fmt.Println(list)
+}
