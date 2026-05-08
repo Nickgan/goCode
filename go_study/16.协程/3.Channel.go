2f223307c0eae6de10b15a0a60dc50eabed5d98d
ganbo <923099645@qq.com>
2026-05-08 20:00:37 +0800

    WaitGroup

---
+++ go_study/16.协程/3.Channel.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+//var ch = make(chan int, 10)
+
+func main() {
+
+	//go func() {
+	//	for i := 0; i < 10; i++ {
+	//		ch <- i
+	//	}
+	//}()
+	//
+	//for i := 0; i < 10; i++ {
+	//	fmt.Println(<-ch)
+	//}
+	//
+	//close(ch)
+
+	var ch chan int
+	ch = make(chan int, 10)
+
+	ch <- 1
+	ch <- 2
+	ch <- 3
+
+	close(ch)
+
+	for i := range ch {
+		fmt.Println(i)
+	}
+}
