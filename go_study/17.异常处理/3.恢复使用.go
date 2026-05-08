058a57d73aa72fafc6b76080ed1e0470114d119f
ganbo <ganbo@qq.com>
2026-05-08 17:49:52 +0800

    异常处理

--- go_study/17.异常处理/3.恢复使用.go
+++ go_study/17.异常处理/3.恢复使用.go
@@ -6,6 +6,19 @@ import (
 
 func read() {
 
+	//defer func() {
+	//	if err := recover(); err != nil {
+	//		//fmt.Println(err)
+	//		fmt.Println("发生异常了，异常信息=====>", err)
+	//	}
+	//}()
+
+	var list = []int{1, 2, 3, 4, 5}
+	fmt.Println(list[10])
+}
+
+func main() {
+
 	defer func() {
 		if err := recover(); err != nil {
 			//fmt.Println(err)
@@ -13,11 +26,6 @@ func read() {
 		}
 	}()
 
-	var list = []int{1, 2, 3, 4, 5}
-	fmt.Println(list[10])
-}
-
-func main() {
 	read()
 
 	fmt.Println("main方法结束。。。")
