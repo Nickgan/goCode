84b16b52237444c0df7d13587bb907ea6ccd4410
ganbo <923099645@qq.com>
2026-05-18 22:23:55 +0800

    参数校验

--- go_gin/11.参数校验.go
+++ go_gin/11.参数校验.go
@@ -4,17 +4,49 @@ import (
 	"fmt"
 	"goCode/go_gin/resp"
 	"net/http"
+	"reflect"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/locales/zh"
+	"github.com/go-playground/universal-translator"
+	"github.com/go-playground/validator/v10"
+	zh_translations "github.com/go-playground/validator/v10/translations/zh"
 )
 
+/*
+*
+* 参考文档： https://www.fengfengzhidao.com/special/3/41
+ */
+
+var trans ut.Translator
+
+func init() {
+	// 创建翻译器
+	uni := ut.New(zh.New())
+	trans, _ = uni.GetTranslator("zh")
+
+	// 注册翻译器
+	v, ok := binding.Validator.Engine().(*validator.Validate)
+	if ok {
+		_ = zh_translations.RegisterDefaultTranslations(v, trans)
+	}
+
+	v.RegisterTagNameFunc(func(field reflect.StructField) string {
+		label := field.Tag.Get("label")
+		if label == "" {
+			return field.Name
+		}
+		return label
+	})
+}
 func main() {
 
 	gin.SetMode(gin.ReleaseMode) //精简日志输出
 	r := gin.Default()
 
 	// 4：json参数
-	r.POST("/user/add/json", func(c *gin.Context) {
+	r.POST("/user/add/json/validata", func(c *gin.Context) {
 		type User struct {
 			Name string `json:"name" binding:"required,min=3,max=5"`
 			Age  int    `json:"age"`
