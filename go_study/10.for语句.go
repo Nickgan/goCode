140e66c357a617b6ea0ea43d2f72eeb7ce81ccbf
ganbo <923099645@qq.com>
2026-06-19 20:15:23 +0800

    参数校验

--- go_study/10.for语句.go
+++ go_study/10.for语句.go
@@ -1,26 +1,84 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
 
-	for1()
+	//for1()
+	f99()
 
 	// 每隔一秒打印时间
-	for {
-		fmt.Println(time.Now().Format("2006_01-02 15:04:05"))
-		time.Sleep(time.Second)
-	}
+	//for {
+	//	fmt.Println(time.Now().Format("2006_01-02 15:04:05"))
+	//	time.Sleep(time.Second)
+	//}
 }
 
 func for1() {
 
-	var sum int
-	for i := 1; i <= 100; i++ {
-		sum += i
+	//var sum int
+	//for i := 1; i <= 100; i++ {
+	//	sum += i
+	//}
+	//fmt.Println(sum)
+
+	// while 模式
+	//i := 0
+	//sum := 0
+	//for i < 100 {
+	//	i++
+	//	sum += i
+	//}
+	//fmt.Println(sum)
+
+	// do while模式
+	//i := 0
+	//sum := 0
+	//for {
+	//	i++
+	//	sum += i
+	//	if i >= 100 {
+	//		break
+	//	}
+	//}
+	//fmt.Println(sum)
+
+	// 遍历切片
+	var list = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	for i := range list {
+		fmt.Println(list[i])
 	}
-	fmt.Println(sum)
+	fmt.Println("===========")
+	for i, s := range list {
+		fmt.Println(i, s)
+	}
+	fmt.Println("===========")
+
+	// 遍历map
+	var nameMap = map[int]string{
+		1: "张三",
+		2: "李四",
+		3: "王五",
+	}
+	for i, s := range nameMap {
+		fmt.Println(i, s)
+	}
+	fmt.Println("===========")
+	for i := range nameMap {
+		fmt.Println(i, nameMap[i])
+	}
+}
+
+// 九九乘法表
+func f99() {
+	for i := 1; i <= 9; i++ {
+		for j := 1; j <= i; j++ {
+			//if j > i {
+			//
+			//	break
+			//}
+			fmt.Printf("%d*%d=%d\t", i, j, i*j)
+		}
+		fmt.Println()
+	}
 }
--- go_study/11.函数.go
+++ go_study/11.函数.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 
-	sum1 := sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 55)
-	fmt.Println("main sum==========>", sum1)
+	//sum1 := sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 55)
+	//fmt.Println("main sum==========>", sum1)
 
 	// 匿名函数
 	//m2 := func() {
@@ -16,8 +16,14 @@ func main() {
 	//ff()
 
 	var n int = 10
+	fmt.Println("main n=", &n)
 	ts1(&n)
 	fmt.Println("main n=", n)
+
+	// %p 打印地址
+	var list = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Printf("list=%p", &list) // %p打印地址
+
 }
 
 func sum(numList ...int) (sum int) {
@@ -26,7 +32,6 @@ func sum(numList ...int) (sum int) {
 
 	for n, i := range numList {
 		sum += i
-
 		fmt.Println("n=", n, "i=", i)
 	}
 	fmt.Println("sum=", sum)
@@ -55,7 +60,16 @@ func ff() {
 
 // &是取地址，*是解引用，去这个地址指向的值
 func ts1(n *int) {
+	fmt.Println("ts1 n=", n)
 	fmt.Println("ts1 n=", *n)
 	*n = 18
 	fmt.Println("ts1 n=", *n)
+}
+
+// 接收一个函数，返回一个函数
+func f111111(a func() (int, string)) (func(), int) {
+	a()
+	return func() {
+		fmt.Println("f111111")
+	}, 1
 }
