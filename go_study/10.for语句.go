6ffaddcaad77ebedd75d811b16d4d3fc46084cb9
ganbo <923099645@qq.com>
2026-06-19 16:31:09 +0800

    参数校验

--- go_study/10.for语句.go
+++ go_study/10.for语句.go
@@ -7,9 +7,20 @@ import (
 
 func main() {
 
+	for1()
+
 	// 每隔一秒打印时间
 	for {
 		fmt.Println(time.Now().Format("2006_01-02 15:04:05"))
 		time.Sleep(time.Second)
 	}
+}
+
+func for1() {
+
+	var sum int
+	for i := 1; i <= 100; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
--- go_study/9.switch语句.go
+++ go_study/9.switch语句.go
@@ -34,20 +34,71 @@ func main1() {
 }
 
 func main() {
-	fmt.Println("请输入你的年龄：")
-	var age int
-	fmt.Scan(&age)
+	//fmt.Println("请输入你的年龄：")
+	//var age int
+	//fmt.Scan(&age)
+	//
+	//switch {
+	//case age <= 0:
+	//	fmt.Println("未出生")
+	//	fallthrough
+	//case age <= 18:
+	//	fmt.Println("未成年")
+	//	fallthrough
+	//case age <= 35:
+	//	fmt.Println("青年")
+	//default:
+	//	fmt.Println("中年")
+	//}
 
+	//mm1()
+	mm2()
+}
+
+func mm1() {
+
+	var age int
+	fmt.Print("请输入你的年龄：")
+	fmt.Scan(&age)
 	switch {
-	case age <= 0:
-		fmt.Println("未出生")
-		fallthrough
 	case age <= 18:
-		fmt.Println("未成年")
-		fallthrough
-	case age <= 35:
+		fmt.Println("未成年..")
+		fmt.Println("未成年122")
+	case age <= 50:
 		fmt.Println("青年")
+		fmt.Println("青年222")
+		fallthrough
 	default:
-		fmt.Println("中年")
+		fmt.Println("老年")
+
+	}
+}
+
+func mm2() {
+	var week int
+	fmt.Print("请输入星期数：")
+	n, er := fmt.Scan(&week)
+
+	if er != nil && n != 1 {
+		fmt.Println("输入错误 err")
+		return
+	}
+	switch week {
+	case 1:
+		fmt.Println("星期一")
+	case 2:
+		fmt.Println("星期二")
+	case 3:
+		fmt.Println("星期三")
+	case 4:
+		fmt.Println("星期四")
+	case 5:
+		fmt.Println("星期五")
+	case 6:
+		fmt.Println("星期六")
+	case 7:
+		fmt.Println("星期日")
+	default:
+		fmt.Println("输入错误")
 	}
 }
